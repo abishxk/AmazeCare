c6a7459ef1f31c36858975c5432becd9f06c4f8f
abishxk <107830484+abishxk@users.noreply.github.com>
2025-06-23 08:40:17 +0530

    Delete AmazeAgain directory

--- AmazeAgain/src/main/java/com/amaze/repository/PatientRepo.java
+++
@@ -1,9 +0,0 @@
-package com.amaze.repository;
-
-import com.amaze.entity.Patients;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatientRepo extends JpaRepository<Patients, Integer> {
-    Patients findByEmail(String email);
-
-}
