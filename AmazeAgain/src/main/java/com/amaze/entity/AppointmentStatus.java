aba9db5e813ff0f605a085bdb3df3761f48a6dd1
abishxk <107830484+abishxk@users.noreply.github.com>
2025-11-22 07:26:50 +0530

    Delete AmazeAgain directory

--- AmazeAgain/src/main/java/com/amaze/entity/AppointmentStatus.java
+++
@@ -1,7 +0,0 @@
-package com.amaze.entity;
-
-public enum AppointmentStatus {
-    scheduled,
-    completed,
-    cancelled
-}
--- AmazeAgain/src/main/java/com/amaze/repository/AdminRepo.java
+++
@@ -1,8 +0,0 @@
-package com.amaze.repository;
-
-import com.amaze.entity.Admin;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepo extends JpaRepository<Admin, Integer> {
-    Admin findByEmail(String email);
-}
